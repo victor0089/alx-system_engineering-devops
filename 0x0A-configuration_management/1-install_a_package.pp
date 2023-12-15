4648815e995e2896b683c4b3c8eec4cd40e7053b
Victor Adly <42696918+victor0089@users.noreply.github.com>
2023-12-16 01:23:38 +0200

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,3 +3,15 @@ package { 'puppet-lint':
   ensure   => '2.5.0',
   provider => 'gem'
 }
+#!/usr/bin/env puppet
+# Ensure the 'python3-pip' package is installed
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Install an especific version of flask (2.1.0)
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+  }
