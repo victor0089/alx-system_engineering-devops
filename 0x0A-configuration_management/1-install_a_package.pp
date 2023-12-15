abb4c4cb205fce3e4b6a966eca68aaa2f3f4e807
Victor Adly <42696918+victor0089@users.noreply.github.com>
2023-12-16 01:36:20 +0200

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,9 +3,9 @@ package { 'python3-pip':
   ensure => installed,
 }
 
-# Install an especific version of flask (2.1.0)
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# Install Flask version 2.1.0
+exec { 'install_flask':
+  command  => '/usr/bin/pip3 install flask==2.1.0',
+  unless   => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
   require  => Package['python3-pip'],
-  }
+}
