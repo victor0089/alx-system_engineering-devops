ac46eb65cf786d61f759bc3dae361aeda19a2fc3
Victor Adly <42696918+victor0089@users.noreply.github.com>
2024-01-16 23:40:19 +0200

    Update 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,43 +1,30 @@
 #!/usr/bin/python3
-"""
-Module to query the Reddit API and return the number of subscribers for a given subreddit.
-"""
-
+"""Function to query a list of all hot posts on a given Reddit subreddit."""
 import requests
 
-def number_of_subscribers(subreddit):
-    """
-    Get the number of subscribers for a given subreddit.
 
-    Args:
-        subreddit (str): The name of the subreddit.
+def recurse(subreddit, hot_list=[], after="", count=0):
+    """Returns a list of titles of all hot posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        return None
 
-    Returns:
-        int: The number of subscribers or 0 if the subreddit is invalid.
-    """
-    # Reddit API endpoint for subreddit information
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    
-    # Set a custom User-Agent to avoid errors related to Too Many Requests
-    headers = {'User-Agent': 'CustomUserAgent'}
-    
-    # Make the GET request
-    response = requests.get(url, headers=headers)
-    
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        # Parse the JSON response
-        data = response.json()
-        
-        # Get the number of subscribers from the parsed data
-        subscribers = data['data']['subscribers']
-        
-        return subscribers
-    elif response.status_code == 404:
-        # If subreddit not found, return 0
-        print(f"Subreddit '{subreddit}' not found.")
-        return 0
-    else:
-        # Handle other errors
-        print(f"Error {response.status_code}: Unable to retrieve data.")
-        return 0
+    results = response.json().get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
+        hot_list.append(c.get("data").get("title"))
+
+    if after is not None:
+        return recurse(subreddit, hot_list, after, count)
+    return hot_list
