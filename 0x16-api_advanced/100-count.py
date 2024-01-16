ee6581722ad6673a21b0fd7a029c91d85377e853
Victor Adly <42696918+victor0089@users.noreply.github.com>
2024-01-17 01:06:08 +0200

    Update 100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -52,4 +52,3 @@ def count_words(subreddit, word_list, after="", count=[]):
                     print("{}: {}".format(word_list[i].lower(), count[i]))
         else:
             count_words(subreddit, word_list, after, count)
-
