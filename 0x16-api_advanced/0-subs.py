703711d9e5c3c28e02a8bd5b4109c49f9c8adbf5
Victor Adly <42696918+victor0089@users.noreply.github.com>
2024-01-16 19:18:04 +0200

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Module to query the Reddit API and return the number of subscribers for a given subreddit.
+Module to query the Reddit API and return 
 """
 import requests
 
