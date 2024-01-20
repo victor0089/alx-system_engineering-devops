f8a07b27c5d2ae2481ad74142a796eca0e61940c
Victor Adly <42696918+victor0089@users.noreply.github.com>
2024-01-20 02:03:51 +0200

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
 """ How many subs? """
-from requests import get
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """ Returns subscriber count of subreddit or 0 """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'user-agent': 'victor/1.0 (by RatioForward4396)'}
+    
+    r = requests.get(url, headers=headers, allow_redirects=False)
 
-    headers = {'user-agent': 'yahoov/1.0 (by RatioForward4396)'}
-
-    r = get(url, headers=headers, allow_redirects=False)
+    print("Response Status Code:", r.status_code)
+    print("Response Content:", r.text)
 
     if r.status_code != 200:
         return 0
 
     try:
         js = r.json()
-
     except ValueError:
         return 0
 
